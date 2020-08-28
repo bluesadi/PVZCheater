5e48c823977c8d1196243ec759a8fe46fd8678a8
bluesadi <liuyibo@stu.scu.edu.cn>
2020-08-28 18:00:25 +0800

    modify definition

--- MemoryUtil.h
+++ MemoryUtil.h
@@ -1,8 +1,8 @@
 #include <windows.h>
 
 #define IMAGE_BASE 0x00400000
-#define SUN_INCREMENT_ADDRESS IMAGE_BASE + 0X000309F0
-#define SUN_AMOUNT_ADDRESS readDword(hPVZProcess,readDword(hPVZProcess, 0x6a9ec0) + 0x768) + 0x5560
+#define SUN_PICKED_UP_ONCE_ADDRESS IMAGE_BASE + 0X000309F0
+#define SUN_ADDRESS readDword(hPVZProcess,readDword(hPVZProcess, 0x6a9ec0) + 0x768) + 0x5560
 
 DWORD readDword(HANDLE hPVZProcess, DWORD address) {
 	DWORD result, dwret;
